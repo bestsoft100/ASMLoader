acd38d4e401eaeb596718145761a6b35e4eadf95
bestsoft100 <49400900+bestsoft100@users.noreply.github.com>
2023-08-20 18:25:12 +0200

    Added more styles

--- src/b100/asmloader/gui/ASMLoaderGUI.java
+++ src/b100/asmloader/gui/ASMLoaderGUI.java
@@ -1,25 +1,30 @@
 package b100.asmloader.gui;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
+import b100.asmloader.gui.style.Style;
 import b100.asmloader.gui.utils.GridPanel;
 
 public class ASMLoaderGUI implements ActionListener {
 	
-	public static void main(String[] args) {
-		new ASMLoaderGUI();
-	}
+	private static List<Style> styles;
+	private static Style currentStyle;
 	
 	public JFrame frame;
 	public GridPanel panel;
@@ -30,8 +35,8 @@ public class ASMLoaderGUI implements ActionListener {
 	
 	public JMenuBar menuBar;
 	public JMenu menuStyle = new JMenu("Style");
-	public JMenuItem menuItemStyleDefault;
-	public JMenuItem menuItemStyleSystem;
+	
+	private Map<Component, Style> componentToStyleMap = new HashMap<>();
 	
 	public ASMLoaderGUI() {
 		frame = new JFrame("ASMLoader GUI");
@@ -64,18 +69,20 @@ public class ASMLoaderGUI implements ActionListener {
 		
 		menuStyle = new JMenu("Style");
 		
-		menuItemStyleDefault = new JRadioButtonMenuItem("Default", !isSystemStyleActive);
-		menuItemStyleSystem = new JRadioButtonMenuItem("System", isSystemStyleActive);
-		
-		menuItemStyleDefault.addActionListener(this);
-		menuItemStyleSystem.addActionListener(this);
-		
-		ButtonGroup buttonGroup = new ButtonGroup();
-		buttonGroup.add(menuItemStyleDefault);
-		buttonGroup.add(menuItemStyleSystem);
-		
-		menuStyle.add(menuItemStyleDefault);
-		menuStyle.add(menuItemStyleSystem);
+		if(styles != null) {
+			ButtonGroup buttonGroup = new ButtonGroup();
+			for(int i=0; i < styles.size(); i++) {
+				Style style = styles.get(i);
+				
+				JRadioButtonMenuItem component = new JRadioButtonMenuItem(style.getName(), style == currentStyle);
+				component.addActionListener(this);
+				buttonGroup.add(component);
+				componentToStyleMap.put(component, style);
+				menuStyle.add(component);
+			}
+		}else {
+			menuStyle.setEnabled(false);
+		}
 		
 		menuBar.add(menuStyle);
 	}
@@ -86,35 +93,50 @@ public class ASMLoaderGUI implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == menuItemStyleDefault) {
-			updateStyle(false);
+		Style style = componentToStyleMap.get(e.getSource());
+		if(style != null) {
+			setStyle(style);
 			frame.setVisible(false);
 			frame.dispose();
 			new ASMLoaderGUI();
-		}
-		if(e.getSource() == menuItemStyleSystem) {
-			updateStyle(true);
-			frame.setVisible(false);
-			frame.dispose();
-			new ASMLoaderGUI();
+			return;
 		}
 	}
 	
-	private static void updateStyle(boolean system) {
+	private void setStyle(Style style) {
 		try {
-			String className;
-			if(system) {
-				className = UIManager.getSystemLookAndFeelClassName();
-			}else {
-				className = UIManager.getCrossPlatformLookAndFeelClassName();
-			}
-			UIManager.setLookAndFeel(className);
-			isSystemStyleActive = system;
-		}catch (Exception ex) {
-			ex.printStackTrace();
+			UIManager.setLookAndFeel(style.getClassName());
+			currentStyle = style;
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	private static boolean isSystemStyleActive;
+	private static void initStyles() {
+		styles = new ArrayList<>();
+		styles.add(new Style("Default", UIManager.getCrossPlatformLookAndFeelClassName()));
+		styles.add(new Style("System Default", UIManager.getSystemLookAndFeelClassName()));
+
+		LookAndFeelInfo[] installedLookAndFeels = UIManager.getInstalledLookAndFeels();
+		for(int i=0; i < installedLookAndFeels.length; i++) {
+			LookAndFeelInfo lookAndFeelInfo = installedLookAndFeels[i];
+			
+			styles.add(new Style(lookAndFeelInfo.getName(), lookAndFeelInfo.getClassName()));
+		}
+		
+		String selectedLookAndFeelClassName = UIManager.getLookAndFeel().getClass().getName();
+		for(int i=0; i < styles.size(); i++) {
+			if(styles.get(i).getClassName().equals(selectedLookAndFeelClassName)) {
+				currentStyle = styles.get(i);
+				break;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		initStyles();
+		
+		new ASMLoaderGUI();
+	}
 
 }
---
+++ src/b100/asmloader/gui/style/Style.java
@@ -0,0 +1,21 @@
+package b100.asmloader.gui.style;
+
+public class Style {
+	
+	private String name;
+	private String className;
+	
+	public Style(String name, String className) {
+		this.name = name;
+		this.className = className;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getClassName() {
+		return className;
+	}
+
+}
